589985d41c661465626c0a4bffb7171e35723c3e
tagiann <115505717+tagiann@users.noreply.github.com>
2022-12-29 01:27:27 +0200

    02 Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0)
+        return 'ERROR'
+    if(num === 0)
+        return ''
+    let newString = string;
+    for (i = 1; i < num; i++){
+        newString += string;
+    }
+    return(newString);
 };
 
 // Do not edit below this line
