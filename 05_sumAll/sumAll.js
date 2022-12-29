e7f901f5d9e7448de2552a8f371fc2dbcf422816
tagiann <115505717+tagiann@users.noreply.github.com>
2022-12-29 15:45:06 +0200

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!(Number.isInteger(num1)) || !(Number.isInteger(num2)))
+        return 'ERROR'
+    if ((num1 < 0) || (num2 < 0))
+        return 'ERROR';
+    let sum = num1 + num2;
+    if (num1 <= num2)
+        for (i = num1 + 1; i < num2; i++)
+            sum += i;
+    else
+        for (i = num2 + 1; i < num1; i++)
+            sum += i;
+    return sum;
 };
 
 // Do not edit below this line
