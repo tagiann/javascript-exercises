5616fc010e45c78eea43834ec0eb766ac268b997
tagiann <115505717+tagiann@users.noreply.github.com>
2023-01-04 00:59:21 +0200

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+  let sum = 0 ;
+  array1.forEach((num) => {
+    sum += num
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array1) {
+  let result = 1;
+  array1.forEach((num) =>{
+    result *= num;
+  })
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0)
+    return 1;
+  let result = 1; 
+  for(let i = 1; i <= num; i++)
+    result *= i;
+  return result;
 };
 
 // Do not edit below this line
