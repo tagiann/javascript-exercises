1b15122ced042ed11a01ec5c2860a29917f4e576
tagiann <115505717+tagiann@users.noreply.github.com>
2023-01-04 11:59:55 +0200

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0)
+        return("OOPS")
+    let n1 = 0;
+    let n2 = 1;
+    let nextTerm;
+    for(i = 1; i <= num; i++){
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return n1;
 };
 
 // Do not edit below this line
